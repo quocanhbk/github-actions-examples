606499a80bafedf74578e14aef56296ae1018c21
La Quoc Anh <hexagon.study@gmail.com>
2022-11-22 13:09:31 +0700

    fix

--- simple/src/app.controller.spec.ts
+++ simple/src/app.controller.spec.ts
@@ -1,7 +1,9 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import * as dotenv from "dotenv";
 
+dotenv.config();
 describe("AppController", () => {
   let appController: AppController;
 
--- simple/src/main.ts
+++ simple/src/main.ts
@@ -1,14 +1,15 @@
-import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { Logger } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log(process.env.NAME);
   await app.listen(process.env.PORT || 3000, () =>
-    Logger.log(
-      `Listening at http://localhost:${process.env.PORT || 3000}`,
-      'Bootstrap',
-    ),
+    Logger.log(`Listening at http://localhost:${process.env.PORT || 3000}`, "Bootstrap")
   );
 }
 bootstrap();
